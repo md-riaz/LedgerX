ecd8bcfbc058c17825b7592c3c3a337ad679cb7b
MD Riaz <mdriaz.wd@gmail.com>
2025-10-31 06:17:33 +0600

    Add platform-aware database connection for web builds

---
+++ lib/data/datasources/connection/connection_web.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:drift/drift.dart';
+import 'package:drift/web.dart';
+
+QueryExecutor createConnection() {
+  final storage = DriftWebStorage.indexedDb('ledgerx_db');
+  return WebDatabase.withStorage(storage);
+}
