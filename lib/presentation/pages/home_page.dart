023804b3435438e18eef960bb9ce9177f7de3e53
MD Riaz <mdriaz.wd@gmail.com>
2025-11-02 06:43:34 +0600

    Show totals on dashboard debt cards

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 import 'package:ledgerx/features/customers/controllers/customer_controller.dart';
+import 'package:ledgerx/domain/entities/entry.dart';
 
 import '../controllers/entry_controller.dart';
 import '../widgets/dashboard_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  static final NumberFormat _currencyFormatter = NumberFormat.currency(
+    locale: 'bn_BD',
+    symbol: '৳',
+    decimalDigits: 2,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -69,14 +77,24 @@ class HomePage extends StatelessWidget {
                     ),
                     DashboardCard(
                       title: 'টাকা পাবো',
-                      count: _getCreditsCount(entryController.entries),
+                      count: _formatAmount(
+                        _calculateTotal(
+                          entryController.entries,
+                          EntryType.credit,
+                        ),
+                      ),
                       icon: Icons.add_circle,
                       color: Colors.teal,
                       onTap: () => Get.toNamed('/entries'),
                     ),
                     DashboardCard(
                       title: 'টাকা দেবো',
-                      count: _getDebitsCount(entryController.entries),
+                      count: _formatAmount(
+                        _calculateTotal(
+                          entryController.entries,
+                          EntryType.debit,
+                        ),
+                      ),
                       icon: Icons.remove_circle,
                       color: Colors.orange,
                       onTap: () => Get.toNamed('/entries'),
@@ -129,12 +147,14 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  String _getCreditsCount(List entries) {
-    return entries.where((e) => e.type.name == 'credit').length.toString();
+  double _calculateTotal(Iterable<Entry> entries, EntryType type) {
+    return entries
+        .where((entry) => entry.type == type)
+        .fold<double>(0, (sum, entry) => sum + entry.amount);
   }
 
-  String _getDebitsCount(List entries) {
-    return entries.where((e) => e.type.name == 'debit').length.toString();
+  String _formatAmount(double amount) {
+    return _currencyFormatter.format(amount);
   }
 
   void _showSearch(BuildContext context) {
